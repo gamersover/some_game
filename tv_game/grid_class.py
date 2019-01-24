bb158fcdc649823292bdf4851fc8b2ca8942f456
gamersover <cmathking@gmail.com>
2019-01-24 15:21:38 +0800

    fix bug

--- tv_game/grid_class.py
+++ tv_game/grid_class.py
@@ -1,15 +1,16 @@
-import pygame
 import sys
+import pygame
 from game_rate import get_grids
 
-#color     R    G    B
+
+# color    R    G    B
 WHITE = [255, 255, 255] 
 BLACK = [  0,   0,   0]
 RED   = [255,   0,   0]
 BLUE  = [  0,   0, 255]
 GREEN = [  0, 255,   0]
 
-#grid num of row or col
+# grid num of row or col
 GRID_NUM = 12
 GAME_LEVEL = 2
 
@@ -24,11 +25,8 @@ class Grid:
         """
         self.screen = screen
         self.screen_size = self.screen.get_size()[0]
-        self.grid_size = self.screen_size/GRID_NUM
-        self.grid_arr = []
-        for i in range(GRID_NUM):
-            for j in range(GRID_NUM):
-                self.grid_arr.append((i,j))
+        self.grid_size = self.screen_size // GRID_NUM
+        self.grid_arr = [(i,j) for i in range(GRID_NUM) for j in range(GRID_NUM)]
         
     def game_start(self, game_rate):
         """
@@ -43,7 +41,9 @@ class Grid:
         """
         self.game_rate = game_rate
         print('you are rate {}'.format(self.game_rate))
+
         self.src_grid, self.trap_grid, self.barr_grid, self.des_grid = get_grids(self.game_rate)
+
         self.terminal = False
         self.game_result = None
     
@@ -67,9 +67,8 @@ class Grid:
     def draw_view(self):
         self.screen.fill(WHITE)
         
-        for i in range(12):
+        for i in range(GRID_NUM):
             pygame.draw.line(self.screen, BLACK, (self.grid_size*(i+1),0), (self.grid_size*(i+1),self.screen_size), 1)
-        for i in range(12):
             pygame.draw.line(self.screen, BLACK, (0,self.grid_size*(i+1)), (self.screen_size,self.grid_size*(i+1)), 1)
         
         for grid in self.grid_arr:
@@ -84,6 +83,7 @@ class Grid:
     
     def step(self, action):
         self.game_over()
+
         if action == 'up':
             self.go_up()
         elif action == 'down':
@@ -92,12 +92,14 @@ class Grid:
             self.go_left()
         elif action == 'right':
             self.go_right()
+
         self.src_grid = sorted(self.src_grid)
         if self.src_grid == self.des_grid:
             self.game_result = 'win'
 
     def go_up(self):
         sort_src_grid = sorted(self.src_grid, key=lambda x:x[1], reverse=False)
+
         for i in range(len(sort_src_grid)):
             new_grid = (sort_src_grid[i][0], sort_src_grid[i][1]-1)
             if new_grid in self.grid_arr:
@@ -107,10 +109,12 @@ class Grid:
                     sort_src_grid[i] = new_grid
                 if new_grid not in self.barr_grid+sort_src_grid[:i]:
                     sort_src_grid[i] = new_grid
+
         self.src_grid = sort_src_grid
     
     def go_down(self):
         sort_src_grid = sorted(self.src_grid, key=lambda x:x[1], reverse=True)
+
         for i in range(len(sort_src_grid)):
             new_grid = (sort_src_grid[i][0], sort_src_grid[i][1]+1)
             if new_grid in self.grid_arr:
@@ -120,10 +124,12 @@ class Grid:
                     sort_src_grid[i] = new_grid
                 if new_grid not in self.barr_grid+sort_src_grid[:i]:
                     sort_src_grid[i] = new_grid
+
         self.src_grid = sort_src_grid        
     
     def go_left(self):
         sort_src_grid = sorted(self.src_grid, key=lambda x:x[0], reverse=False)
+
         for i in range(len(sort_src_grid)):
             new_grid = (sort_src_grid[i][0]-1, sort_src_grid[i][1])
             if new_grid in self.grid_arr:
@@ -133,10 +139,12 @@ class Grid:
                     sort_src_grid[i] = new_grid
                 if new_grid not in self.barr_grid+sort_src_grid[:i]:
                     sort_src_grid[i] = new_grid
+
         self.src_grid = sort_src_grid
     
     def go_right(self):
         sort_src_grid = sorted(self.src_grid, key=lambda x:x[0], reverse=True)
+
         for i in range(len(sort_src_grid)):
             new_grid = (sort_src_grid[i][0]+1, sort_src_grid[i][1])
             if new_grid in self.grid_arr:
@@ -146,4 +154,5 @@ class Grid:
                     sort_src_grid[i] = new_grid
                 if new_grid not in self.barr_grid+sort_src_grid[:i]:
                     sort_src_grid[i] = new_grid
+                    
         self.src_grid = sort_src_grid
--- tv_game/run_game.py
+++ tv_game/run_game.py
@@ -1,6 +1,7 @@
-import pygame
 import sys
+import pygame
 from grid_class import Grid
+
 
 SCREEN_SIZE = (400, 400)
 pygame.init()
@@ -9,23 +10,28 @@ screen = pygame.display.set_mode(SCREEN_SIZE, 0, 32)
 
 def run(game_rate):
     grid.game_start(game_rate)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+
             elif event.type == pygame.KEYDOWN:
                 action = None
                 if event.key == pygame.K_r:
                     print("push r button")
                     grid.reset_game()
                     break
+
                 if event.key == pygame.K_q:
                     print('quit game')
                     sys.exit(0)
+
                 if event.key == pygame.K_s:
                     game_rate = int(input("please input new game rate(1-2):"))
                     grid.reset_game(game_rate)
                     break
+
                 if event.key == pygame.K_UP:
                     action = 'up'
                 if event.key == pygame.K_DOWN:
@@ -35,27 +41,29 @@ def run(game_rate):
                 if event.key == pygame.K_RIGHT:
                     action = 'right'
                 grid.step(action)
+
         grid.draw_view()
         pygame.display.update()
 
+
 if __name__ == '__main__':
     print("#----------------------------------------------------------------------------------#")
-    print("#game info:                                                                        #")
-    print("#   red grid: source                                                               #")
-    print("#   blue grid: destination                                                         #")
-    print("#   black grid: barrier                                                            #")
-    print("#   green grid: trap                                                               #")
+    print("#game info:|                                                                       #")
+    print("#-----------                     red grid: source                                  #")
+    print("#                                blue grid: destination                            #")
+    print("#                                black grid: barrier                               #")
+    print("#                                green grid: trap                                  #")
     print("#----------------------------------------------------------------------------------#")
-    print("                                                                                    ")
-    print("#how to play:                                                                      #")
-    print("# use keyboard up down left right to control red to the blue, and don't touch green#") 
-    print("#                                                                                  #")
-    print("# some tips                                                                        #")
-    print("#   press 'r' to reset                                                             #")
-    print("#   press 's' to select level                                                      #")
-    print("#   press 'q' to quit                                                              #")
-    print("#                                    Good luck!!                                   #")
+    print("#how to play:|                                                                     #")
+    print("#------------      use keyboard up down left right to control                      #") 
+    print("#                  red to the blue, and don't touch green                          #") 
+    print("#----------------------------------------------------------------------------------#")
+    print("#some tips:|                                                                       #")
+    print("#----------                     press 'r' to reset                                 #")
+    print("#                               press 's' to select level                          #")
+    print("#                               press 'q' to quit                                  #")
+    print("#----------------------------------------------------------------------------------#")
+    print("#                                   Good luck!!                                    #")
     grid = Grid(screen)
     game_rate = 1
-    run(game_rate)
-        
+    run(game_rate)    
